dd45ff3ae0eaecd3bc5999a0eb3214537f600822
tomasrobino <tomaserobino@gmail.com>
2024-02-18 20:12:34 +0100

    express setup

--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
-const express = require("express");
 const mysql = require("mysql2");
+const express = require("express");
+const app = express();
+const port = 8000;
+
+
+app.get("/", (req, res) => {
+    res.send("");
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port "+port);
+})
+
 
