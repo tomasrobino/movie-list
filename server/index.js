730df419ada35ce35e11bf3ee7f9dda7833f7565
tomasrobino <tomaserobino@gmail.com>
2024-02-18 20:56:22 +0100

    basic sql connection

--- server/index.js
+++ server/index.js
@@ -1,15 +1,35 @@
-const mysql = require("mysql2");
+require("dotenv").config({path: "../.env"});
+const mysql = require("mysql2/promise");
 const express = require("express");
 const app = express();
 const port = 8000;
 
+console.log(process.env.HOST)
 
-app.get("/", (req, res) => {
-    res.send("");
-});
+async function server() {
+    app.get("/", (req, res) => {
+        res.send("");
+    });
+    
+    app.listen(port, () => {
+        console.log("Server listening on port "+port);
+    });
+    
+    const connection = await mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USER,
+        database: process.env.DATABASE,
+        password: process.env.PASSWORD
+    });
+    
+    try {
+        const [results] = await connection.query(
+            'select * from list'
+        );
+        console.log(results);
+    } catch (err) {
+        console.log(err);
+    }
+}
 
-app.listen(port, () => {
-    console.log("Server listening on port "+port);
-})
-
-
+server();
